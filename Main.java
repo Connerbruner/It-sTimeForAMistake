a67ede466b3c6f632ea01e03fa918db1a49aa3e8
ConnerBrunner <Con-bru000@g.rentonschools.us>
2022-09-01 15:17:19 -0700

    method but better

--- Main.java
+++ Main.java
@@ -2,15 +2,15 @@ class Main {
     public static void main(String[] args) {
         Neuron test = new Neuron(
         new double[][] {
-            {1, 0, 0},
-            {1, 0, 0},
-            {1, 0, 0}
+            {1, -0.5, 0},
+            {1, 0.5, 1},
+            {1, 1, 0}
         },
         new double[][] {
-            {0.3, 0.1, 0.3},
-            {0.1, 0.2, 0.1},
+            {0.5, 0.1, 0.3},
+            {0.5, 0.3, 0.1},
             {0.3, 0.1, 0.3}
         });
-        System.out.print(test.funnyfunction);
+        System.out.print(test.test() );
     }
 }
--- Neuron.java
+++ Neuron.java
@@ -1,19 +1,29 @@
 import static java.lang.Math.tanh;
 
 class Neuron {
-    double funnyfunction = 0;
-    public Neuron(double[][] valArr, double[][] weightArr) {
-        double[][] weightedValues = new double[valArr.length][weightArr.length];
-        for (int i = 0; i < weightedValues.length; i++) {
-            for (int j = 0; j < weightedValues[i].length; j++) {
-                weightedValues[i][j] = valArr[i][j] * weightArr[i][j];
+    double Confidence = 0;
+
+    long completeSpeed;
+    public Neuron( double[][] valArr , double[][] weightArr ) {
+        long start=System.nanoTime();
+        double[][] weightedValues = new double[ valArr.length ][ weightArr.length ];
+        for ( int i = 0 ; i < weightedValues.length ; i++ ) {
+            for ( int j = 0 ; j < weightedValues[ i ].length ; j++ ) {
+                weightedValues[ i ][ j ] = valArr[ i ][ j ] * weightArr[ i ][ j ];
             }
         }
-        for (int i = 0; i < weightedValues.length; i++) {
-            for (int j = 0; j < weightedValues[i].length; j++) {
-                funnyfunction += weightedValues[i][j];
+        for ( int i = 0 ; i < weightedValues.length ; i++ ) {
+            for ( int j = 0 ; j < weightedValues[ i ].length ; j++ ) {
+                Confidence += weightedValues[ i ][ j ];
             }
         }
-        funnyfunction = tanh(funnyfunction);
+        completeSpeed=System.nanoTime()-start;
+    }
+    public double getConfidence()
+    {
+        return Confidence = tanh( Confidence );
+    }
+    public String test() {
+        return "Confidence: "+(Confidence = tanh( Confidence ))+"\nTime to complete (nanos): "+completeSpeed;
     }
 }
