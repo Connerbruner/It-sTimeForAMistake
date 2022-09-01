6c7a69d98dce22adf8ab9e2cfb00c919b740fbee
Conner Brunner <72578768+Connerbruner@users.noreply.github.com>
2022-09-01 19:05:13 +0000

    its time to make code nice

--- Testing.java
+++ Testing.java
@@ -1,3 +1,17 @@
-public Testing {
-    //for static testing stuff or other helpful methods
+import java.awt.*;
+import java.awt.image.BufferedImage;
+class Testing {
+     
+    public BufferedImage createPlate() {
+        int width = 0;
+        int height = 0;
+        BufferedImage plate = new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+               
+                    plate.setRGB(j, i, new Color(0,0,0).getRGB()); 
+            }
+        }
+        return plate;
+    }
 }
